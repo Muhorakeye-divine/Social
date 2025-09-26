4653bd93d4693c5620dd52498821f84296c276d3
Muhorakeye-divine <muhorakeyediv@gmail.com>
2025-09-26 17:04:12 +0200

    troisieme commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Apiairline } from './apiairline/apiairline';
 import { Home } from './home/home';
 
+
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home},
   { path: 'api', component: Apiairline },
+ 
 
      
 ];
